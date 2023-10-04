9d1b2207fe56a3458feca4d646e4f672419d3a66
kaedmamfc <macabanticruz123@gmail.com>
2023-10-04 11:06:12 +0800

    cs

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
  import AnalyticsView from '../views/AnalyticsView.vue'
 import TestingView from '../views/TestingView.vue'
+import ChatbotSettingsView from '../views/ChatbotSettingsView.vue'
 import KnowledgeSourceView from '../views/KnowledgeSourceView.vue'
 
 const router = createRouter({
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/testing',
       name: 'testing',
       component: TestingView
+    },
+    {
+      path: '/chatbot-settings',
+      name: 'chatbot-settings',
+      component: ChatbotSettingsView
     },
     {
       path: '/knowledge-source',
