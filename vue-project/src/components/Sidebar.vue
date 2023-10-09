904ebf3bfd2a45e566315133a488932274edccf6
CaptainStone123 <146410635+CaptainStone123@users.noreply.github.com>
2023-10-09 22:05:56 +0800

    Update Sidebar.vue

--- vue-project/src/components/Sidebar.vue
+++ vue-project/src/components/Sidebar.vue
@@ -130,7 +130,7 @@ export default {
         axios.get('http://localhost:5000/api/getImageFromMongoDB')
             .then(response => {
                 const imageName = response.data.Image || 'stacy.png'; 
-                const imageSrc = `./vue-project/src/assets/${imageName}`;
+                const imageSrc = `../assets/${imageName}`;
 
                 console.log(imageSrc);
                 
